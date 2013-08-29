6e619415ecf219aeabee89b0912700dca7b38dbd
Dan Haeberlein <dan.haeberlein@googlemail.com>
2013-08-29 19:57:16 +0200

    deleted className attribute from JmxMBeanData

--- src/main/groovy/de/tiq/jmx/JmxMBeanData.groovy
+++ src/main/groovy/de/tiq/jmx/JmxMBeanData.groovy
@@ -4,22 +4,14 @@ import javax.management.ObjectName
 
 class JmxMBeanData {
 	
-	String className;
 	ObjectName associatedObjectName;
 	List<String> attributes;
-	
-	JmxMBeanData(String className, ObjectName associatedObjectName, List<String> applyableMethodNames) {
-		this.className = className
-		this.associatedObjectName = associatedObjectName
-		this.attributes = applyableMethodNames
-	}
 	
 	@Override
 	public boolean equals(Object obj) {
 		boolean isEqual = false
 		if(obj instanceof JmxMBeanData){
-			isEqual = obj.className == className && 
-					  obj.associatedObjectName == associatedObjectName && 
+			isEqual = obj.associatedObjectName == associatedObjectName && 
 					  obj.attributes == attributes
 		}
 		return isEqual
@@ -27,7 +19,7 @@ class JmxMBeanData {
 
 	@Override
 	public String toString() {
-		return "JmxMBeanData [className=" + className + ", associatedObjectName=" + associatedObjectName + ", applyableMethodNames=" + attributes.join(" ") + "]";
+		return "JmxMBeanData [associatedObjectName=" + associatedObjectName + ", attributes=" + attributes + "]";
 	}
 	
 }
--- src/main/groovy/de/tiq/jmx/XmlToJmxBeanDataConverter.groovy
+++ src/main/groovy/de/tiq/jmx/XmlToJmxBeanDataConverter.groovy
@@ -15,13 +15,10 @@ class XmlToJmxBeanDataConverter {
 		def xmlContent = new File(pathToFile).getText('UTF-8')
 		def jmxMBeanDataList = new XmlParser().parseText(xmlContent)
 		jmxMBeanDataList.each { node ->
-			def className = node.get("className").text()
 			def associatedMethodName = node.get("associatedObjectName").text()
 			//the get method of a node returns a nodelist, so we need to get the first value of it with [0]
 			def applyableMethods = node.get("attributes")[0].children().collect {it.text()}	
-			result << new JmxMBeanData(className, 
-							 		   new ObjectName(associatedMethodName), 
-							 		   applyableMethods) 
+			result << new JmxMBeanData(associatedObjectName:new ObjectName(associatedMethodName), attributes:applyableMethods) 
 		}
 		return result
 	}
